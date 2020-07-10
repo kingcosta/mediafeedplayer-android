627fe5bd5de9a8be494a1f3b6d3f4b926fc6e250
Joseph Peng <jppappstudio@gmail.com>
2020-07-10 13:20:05 +0800

    commit from dev branch

--- app/src/main/java/com/jppappstudio/mediafeedplayer/android/MainActivity.kt
+++ app/src/main/java/com/jppappstudio/mediafeedplayer/android/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Main Activity OnCreate Triggered")
+        println("Another println")
 
         if (savedInstanceState == null) {
             setupBottomNavigationBar()
